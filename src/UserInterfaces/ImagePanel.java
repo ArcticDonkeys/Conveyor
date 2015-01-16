0e81b7359d65b22ddead4c457b52c16e6daade48
onrylmz <onur.yilmzz@gmail.com>
2015-01-16 16:05:24 +0200

    ImagePanel.java is added.

---
+++ src/UserInterfaces/ImagePanel.java
@@ -0,0 +1,28 @@
+package UserInterfaces;
+
+class ImagePanel extends JPanel {
+
+	  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Image img;
+
+	  public ImagePanel(String img) {
+	    this(new ImageIcon(img).getImage());
+	  }
+
+	  public ImagePanel(Image img) {
+	    this.img = img;
+	    Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
+	    setPreferredSize(size);
+	    setMinimumSize(size);
+	    setMaximumSize(size);
+	    setSize(size);
+	    setLayout(null);
+	  }
+
+	  public void paintComponent(Graphics g) {
+	    g.drawImage(img, 0, 0, null);
+	  }
+	}
--- src/UserInterfaces/UserInterface.java
+++ src/UserInterfaces/UserInterface.java
@@ -24,32 +24,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
-class ImagePanel extends JPanel {
 
-	  /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private Image img;
-
-	  public ImagePanel(String img) {
-	    this(new ImageIcon(img).getImage());
-	  }
-
-	  public ImagePanel(Image img) {
-	    this.img = img;
-	    Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
-	    setPreferredSize(size);
-	    setMinimumSize(size);
-	    setMaximumSize(size);
-	    setSize(size);
-	    setLayout(null);
-	  }
-
-	  public void paintComponent(Graphics g) {
-	    g.drawImage(img, 0, 0, null);
-	  }
-	}
 
 public class UserInterface {
 
